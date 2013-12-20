fd614c02147996c76b95523a733acfb938b93d99
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-12-20 10:14:55 +0000

    Fix twitter carousel error + tidy code

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,32 +2,34 @@ $(document).ready(function(){
     /*
     *   Global variables.
     */
-    var pageHeight = $(window).height();
-    var pageWidth = $(window).width();
-    var navigationHeight = $("#navigation").outerHeight();
+    var pageHeight = $(window).height()
+    var pageWidth = $(window).width()
+    var navigationHeight = $('#navigation').outerHeight()
 
     /*
     *   ON RESIZE, check again
     */
     $(window).resize(function () {
-        pageWidth = $(window).width();
-        pageHeight = $(window).height();
-    });
-    
+        pageWidth = $(window).width()
+        pageHeight = $(window).height()
+    })
+
     /*
     *   ON LOAD
     */
 
     // Fix navigation.
-    $('#navigation').fixedonlater();
-    
+    $('#navigation').fixedonlater()
+
     //Initialize scroll so if user droped to other part of page then home page.
-    $(window).trigger('scroll');
-   
-    // Carousel "Quote slider" initialization.
-    $('#quote-slider').carousel({
-        interval: 20000
-    })
+    $(window).trigger('scroll')
+
+    // Carousel 'Quote slider' initialization.
+    if (document.getElementById('quote-slider')) {
+        $('#quote-slider').carousel({
+            interval: 20000
+        })
+    }
 
     //Scroll spy and scroll filter
     $('#main-menu').onePageNav({
@@ -38,38 +40,37 @@ $(document).ready(function(){
         scrollSpeed: 750,
         filter: '',
         easing: 'swing',
-     });
-    
-    
+     })
+
+
     // Paralax initialization.
     // Exclude for mobile.
-    if(pageWidth > 980){
-        //Dont user paralax for tablet and mobile devices.
-        $('#page-welcome').parallax("0%", 0.2);
-        $('#page-features').parallax("0%", 0.07);
-        $('#page-twitter').parallax("0%", 0.1);
+    if (pageWidth > 980) {
+        // Dont user paralax for tablet and mobile devices.
+        $('#page-welcome').parallax('0%', 0.2)
+        $('#page-twitter').parallax('0%', 0.1)
     }
 
     //Emulate touch on table/mobile touchstart.
     if(typeof(window.ontouchstart) != 'undefined') {
-        var touchElements = [".social-icons a", ".portfolio-items li", ".about-items .item"];
+        var touchElements = ['.social-icons a', '.menu-items li', '.about-items .item']
 
         $.each(touchElements, function (i, val) {
             $(val).each(function(i, obj) {
-                $(obj).bind('click', function(e){
+                $(obj).bind('click', function(e) {
 
-                    if($(this).hasClass('clickInNext')){
-                        $(this).removeClass('clickInNext');
+                    if($(this).hasClass('clickInNext')) {
+                        $(this).removeClass('clickInNext')
                     } else {
-                        e.preventDefault();
-                        e.stopPropagation();
+                        e.preventDefault()
+                        e.stopPropagation()
 
-                        $(this).mouseover();
-                        $(this).addClass('clickInNext');
+                        $(this).mouseover()
+                        $(this).addClass('clickInNext')
                     }
-                });
-            });
-        });
+                })
+            })
+        })
     }
 
     /*
@@ -79,19 +80,19 @@ $(document).ready(function(){
     *   Main menu links
     *   Logo click on Welcome page
     */
-    $('#page-welcome .logo a').click(function(){
+    $('#page-welcome .logo a').click(function() {
         $('html, body').animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top - navigationHeight + 4
+            scrollTop: $($.attr(this, 'href')).offset().top - navigationHeight + 4
 
-        }, 800);
+        }, 800)
 
-        //Fix jumping of navigation.
+        // Fix jumping of navigation.
         setTimeout(function() {
-            $(window).trigger('scroll');
-        }, 900);
+            $(window).trigger('scroll')
+        }, 900)
 
-        return false;
-    });
+        return false
+    })
 
 
     /*
@@ -109,7 +110,8 @@ $(document).ready(function(){
         controls: false,
         useCSS: false,
         pause: 10000
-    });
+    })
+
     /*
     *   PAGE | WORK
     *
@@ -117,8 +119,8 @@ $(document).ready(function(){
     *   .plugin-filter-elements - Defines items with li.
     */
     $('.plugin-filter').click(function(){
-        return false;
-    });
+        return false
+    })
     $('.plugin-filter-elements').mixitup({
         targetSelector: '.mix',
         filterSelector: '.plugin-filter',
@@ -146,7 +148,7 @@ $(document).ready(function(){
         onMixLoad: null,
         onMixStart: null,
         onMixEnd: null
-    });
+    })
 
     /*
     *   PAGE | Twitter
@@ -160,32 +162,35 @@ $(document).ready(function(){
         modpath: 'plugins/twitter/',
         username: 'TheGridelicious',
         count: 3
-    });
+    })
     */
 
-    //Prepare markup for twitter feed and carousel. Alow twitter to load. 1s, load time.
+    // Prepare markup for twitter feed and carousel. Alow twitter to load. 1s, load time.
     setTimeout(function() {
-        var myCarousel = $("#twitterfeed-slider");
-        myCarousel.append("<ol class='carousel-indicators'></ol>");
+        var myCarousel = $('#twitterfeed-slider')
+        myCarousel.append("<ol class='carousel-indicators'></ol>")
 
-        myCarousel.find('.tweet_list').addClass("carousel-inner");
-        myCarousel.find('.tweet_list li').addClass('item').first().addClass("active");
+        myCarousel.find('.tweet_list').addClass('carousel-inner')
+        myCarousel.find('.tweet_list li').addClass('item').first().addClass('active')
 
-        var indicators = myCarousel.find(".carousel-indicators");
-        myCarousel.find(".carousel-inner").children(".item").each(function(index) {
+        var indicators = myCarousel.find('.carousel-indicators')
+        myCarousel.find('.carousel-inner').children('.item').each(function(index) {
             (index === 0) ?
             indicators.append("<li data-target='#twitterfeed-slider' data-slide-to='"+index+"' class='active'></li>") :
-            indicators.append("<li data-target='#twitterfeed-slider' data-slide-to='"+index+"'></li>");
-        });
+            indicators.append("<li data-target='#twitterfeed-slider' data-slide-to='" + index + "'></li>")
+        })
 
-        //After creating markup, start carousel.
-       $('#twitterfeed-slider').carousel({
-            interval: 5000,
-            pause: "hover"
-        });
+       // After creating markup, start carousel.
+       if (document.getElementById('twitterfeed-slider')
+           && (0 !==  document.getElementById('twitterfeed-slider').firstChild.children.length)) {
+           $('#twitterfeed-slider').carousel({
+                interval: 5000,
+                pause: "hover"
+            })
+       }
 
-    }, 1000);
-});
+    }, 1000)
+})
 
 
 /*
@@ -193,37 +198,36 @@ $(document).ready(function(){
 *   Start loading.
 *   Append loading notification.
 */
-$( document ).ajaxSend( function() {
+$(document).ajaxSend( function() {
 
     // Show loader.
-    if($(".loading").length == 0) {
-        $("body").append('<div class="loading"><div class="progress progress-striped active"><div class="bar"></div></div></div>');
-        $(".loading").slideDown();
-        $(".loading .progress .bar").delay(300).css("width", "100%");
+    if($('.loading').length == 0) {
+        $("body").append('<div class="loading"><div class="progress progress-striped active"><div class="bar"></div></div></div>')
+        $('.loading').slideDown()
+        $('.loading .progress .bar').delay(300).css('width', '100%')
     }
-});
+})
 
 /*
 *   Reinitialize Scrollspy after ajax request is completed.
 *   Refreshing will recalculate positions of each page in document.
 *   Time delay is added to allow ajax loaded content to expand and change height of page.
 */
-$( document ).ajaxComplete(function() {
+$(document).ajaxComplete(function() {
 
     // Remove loading section.
-    $(".loading").delay(1000).slideUp(500, function(){
-        $(this).remove();
-    });
+    $('.loading').delay(1000).slideUp(500, function(){
+        $(this).remove()
+    })
 
 
-    // Portfolio details - close.
-    $(".close-portfolio span").click(function(e) {
-        $(".portfolio-item-details").delay(500).slideUp(500, function(){
-            $(this).remove();
-        });
+    // Menu details - close.
+    $('.close-menu span').click(function(e) {
+        $('.menu-item-details').delay(500).slideUp(500, function() {
+            $(this).remove()
+        })
 
-        window.location.hash= "!";
-        return false;
-    });
-});
-
+        window.location.hash= '!'
+        return false
+    })
+})
